a1d55a1ebf9ca597c84bfca3dc719067c061fbc7
David Heaton <david.heaton@workday.com>
2016-01-16 22:53:41 +0000

    Update layout to material design spec

--- app/src/main/java/org/dave3heaton/rpgdiceroller/RpgDiceRollerMain.java
+++ app/src/main/java/org/dave3heaton/rpgdiceroller/RpgDiceRollerMain.java
@@ -16,17 +16,7 @@ public class RpgDiceRollerMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rpg_dice_roller_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
