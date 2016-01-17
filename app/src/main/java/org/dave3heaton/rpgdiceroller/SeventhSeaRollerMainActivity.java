7a9333956751e0647e2498f0e1d42d0516efe21a
David Heaton <david.heaton@workday.com>
2016-01-17 22:05:11 +0000

    Selection of dice to roll enforces maximum dice to keep

--- app/src/main/java/org/dave3heaton/rpgdiceroller/SeventhSeaRollerMainActivity.java
+++ app/src/main/java/org/dave3heaton/rpgdiceroller/SeventhSeaRollerMainActivity.java
@@ -1,14 +1,14 @@
 package org.dave3heaton.rpgdiceroller;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeventhSeaRollerMainActivity extends AppCompatActivity {
 
@@ -16,12 +16,14 @@ public class SeventhSeaRollerMainActivity extends AppCompatActivity {
     private RadioGroup rollGroup;
     private RadioGroup keepGroup;
 
+    private static final String LOG_CATEGORY = "SeventhSeaRoller";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seventh_sea_roller_main);
 
-        Log.i("SeventhSeaActivity", "Activity starting up: registering listeners");
+        Log.d(LOG_CATEGORY, "Activity starting up: registering listeners");
 
         // Initialise the listeners on the 'Roll' radiogroup
         rollGroup = (RadioGroup)findViewById(R.id.seventh_sea_roller_roll_group);
@@ -29,7 +31,7 @@ public class SeventhSeaRollerMainActivity extends AppCompatActivity {
         rollGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                Log.i("SeventhSeaActivity", "Roll group button press: " + checkedId);
+                updateRollGroup(checkedId);
             }
         });
 
@@ -39,10 +41,90 @@ public class SeventhSeaRollerMainActivity extends AppCompatActivity {
         keepGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                Log.i("SeventhSeaActivity", "Keep group button press: " + checkedId);
+                Log.d(LOG_CATEGORY, "Keep group button press: " + checkedId);
             }
         });
 
     }
+
+    private void updateRollGroup(int checkedButtonId) {
+        Log.d(LOG_CATEGORY, "Roll group state change: " + checkedButtonId);
+        Log.d(LOG_CATEGORY, "Roll group, button " + getDiceToRoll() + " pressed.");
+
+        setMaximumKeepValue(getDiceToRoll());
+    }
+
+    private int getDiceToRoll() {
+        return getValueFromButtonGroup(rollGroup);
+    }
+
+    private int getDiceToKeep() {
+        return getValueFromButtonGroup(keepGroup);
+    }
+
+    private int getValueFromButtonGroup(RadioGroup radioGroup) {
+        RadioButton buttonSelected = (RadioButton)findViewById(radioGroup.getCheckedRadioButtonId());
+        String buttonText = buttonSelected.getText().toString();
+        return Integer.parseInt(buttonText);
+    }
+
+    private int getValueFromButtonId(int buttonId) {
+        RadioButton buttonSelected = (RadioButton)findViewById(buttonId);
+        String buttonText = buttonSelected.getText().toString();
+        return Integer.parseInt(buttonText);
+    }
+
+    private void setMaximumKeepValue(int maximumKeepValue) {
+        Log.d(LOG_CATEGORY, "Setting maximum keep value to " + maximumKeepValue);
+        boolean resetDiceToKeep = getDiceToKeep() > maximumKeepValue;
+
+        List<RadioButton> buttonList = getRadioButtonListFromGroup(keepGroup);
+
+        if(resetDiceToKeep) {
+            Log.i(LOG_CATEGORY, "Dice to keep exceeds dice to roll, resetting.");
+            keepGroup.clearCheck();
+            for (RadioButton button : buttonList) {
+                if (getValueFromButtonId(button.getId()) > maximumKeepValue) {
+                    button.setChecked(false);
+                    button.setSelected(false);
+                }
+            }
+        }
+
+
+
+        for (RadioButton button : buttonList) {
+            // Disable all buttons for values higher than the maximum keep value, enable all others.
+            if (getValueFromButtonId(button.getId()) > maximumKeepValue) {
+                Log.d(LOG_CATEGORY, "Disabling button " + getValueFromButtonId(button.getId()));
+                button.setEnabled(false);
+            } else {
+                Log.d(LOG_CATEGORY, "Enabling button " + getValueFromButtonId(button.getId()));
+
+                button.setEnabled(true);
+            }
+
+            if (resetDiceToKeep && getValueFromButtonId(button.getId()) == maximumKeepValue) {
+                Log.d(LOG_CATEGORY, "Checking new keep value.");
+                button.setChecked(true);
+            }
+
+
+
+        }
+        Log.d(LOG_CATEGORY, "New keep value is " + getDiceToKeep());
+    }
+
+    private List<RadioButton> getRadioButtonListFromGroup(RadioGroup radioGroup) {
+        List<RadioButton> buttonList = new ArrayList<>();
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            View potentialRadioButton = radioGroup.getChildAt(i);
+            if (potentialRadioButton instanceof RadioButton) {
+                buttonList.add((RadioButton)potentialRadioButton);
+            }
+        }
+        return buttonList;
+    }
+
 
 }
